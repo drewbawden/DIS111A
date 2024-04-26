c92ff84598d9f3f1fcdc7460e1165f4888c82287
Drew Bawden <drewbawden1234@gmail.com>
2024-04-26 15:46:49 +1000

    Added tuckshop navigation and small modifications

--- websiteFIA2/js/search.js
+++ websiteFIA2/js/search.js
@@ -2,17 +2,17 @@ let searchInput;
 
 window.addEventListener('DOMContentLoaded', function() {
     searchInput = document.getElementById("searchInput");
-    searchInput.addEventListener("keydown", function (e) {
-        if (e.code == "Enter") {
+    searchInput.addEventListener("keydown", function (key) {
+        if (key.code == "Enter") {
             searchWebsite();
         }
     });
 });
 
 function searchWebsite(){
-    // I'm not gonna include any regex so try not to do anything bad ;)
     let baseURL = "https://toolooashs.eq.edu.au/sitesearch/Pages/results.aspx?k=";
     let searchQuery = searchInput.value;
     
+    // I'm not gonna include any regex, their fault if they do something bad
     window.location.href = baseURL + searchQuery;
 }
